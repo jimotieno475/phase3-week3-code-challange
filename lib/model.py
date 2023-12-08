00657ccb83459989c98ac396e03854b2d6051674
jim <jim.otieno@student.moringaschool.com>
2023-12-08 13:31:07 +0300

    still in process

--- lib/model.py
+++ lib/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey,MetaData
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship,session
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -20,6 +20,16 @@ class Restaurant(Base):
         return f'Restaurant(id={self.id},'+\
             f'name={self.name},'+\
             f'price={self.price})'
+    
+    def all_reviews(self):
+        reviews = []
+        for review in self.reviews:
+            customer_name = f"{review.customer.first_name} {review.customer.last_name}"
+            review_text = f"Review for {self.name} by {customer_name}: {review.star_rating} stars."
+            reviews.append(review_text)
+        return reviews
+    def fanciest(cls):
+        return session.query(cls).order_by(cls.price.desc()).first()
 
 class Customer(Base):
     __tablename__ = 'customers'
@@ -32,6 +42,34 @@ class Customer(Base):
         return f'Customer(id={self.id},'+\
             f'first_name={self.first_name},'+\
             f'last_name={self.last_name})'
+    def full_name(self):
+        full=f'{self.first_name}{self.last_name}'
+        return full
+    def favorite_restaurant(self):
+        # Get all reviews by this customer
+        customer_reviews = [review for review in self.reviews]
+        
+        # Find the highest rated restaurant among the customer's reviews
+        if customer_reviews:
+            highest_rated_review = max(customer_reviews, key=lambda x: x.star_rating)
+            return highest_rated_review.restaurant
+        else:
+            return None 
+        
+    def add_review(self, restaurant, rating):
+        new_review = Review(restaurant=restaurant, customer=self, star_rating=rating)
+        session.add(new_review)
+        session.commit()
+
+    def delete_reviews(self, restaurant):
+        # Get all reviews by this customer for the specified restaurant
+        reviews_to_delete = session.query(Review).filter_by(customer_id=self.id, restaurant_id=restaurant.id).all()
+        
+        # Delete the reviews
+        for review in reviews_to_delete:
+            session.delete(review)
+        session.commit()
+
     
 class Review(Base):
     __tablename__ = 'reviews'
@@ -45,3 +83,10 @@ class Review(Base):
     def __repr__(self):
         return f'Review(id={self.id},'+\
             f'star_rating={self.star_rating})'
+    # def full_review(self):
+    #     return f'Review for {Restaurant.name} by {Customer.first_name}+{Customer.last_name}: {Review.star_rating} stars'
+    #     #return f"Review for {self.restaurant.name} by {self.customer.first_name} {self.customer.last_name}: {self.star_rating} stars"
+    def full_review(self):
+        customer_name = f"{self.customer.first_name} {self.customer.last_name}"
+        return f"Review for {self.restaurant.name} by {customer_name}: {self.star_rating} stars"
+
